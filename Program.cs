00eda923eeb3cb98a05a820069b568a0ed5ce374
Никита <samnikita141204@yandex.ru>
2022-12-05 18:46:43 +0400

    Сщхранил задачу 52, среднее значение по столбцам

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ void PrintArray(double[,] array)
 double[,] massiv=FillArrayRamdom(3, 4, -10, 10);
 PrintArray(massiv);*/
 
-//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 //Например, задан массив:
 
@@ -110,4 +110,73 @@ void SearchAddress(int[,] array)
 (int rows, int cols) parametersWork=ParametersRandomArray();
 int[,] massiv=FillArrayRamdom(parametersWork.rows, parametersWork.cols, 0, 10);
 PrintArray(massiv);
-SearchAddress(massiv);
+SearchAddress(massiv);*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+using static System.Console;
+Clear();
+
+( int rows,int cols) ParametersRandomArray()
+{
+    (int rows, int cols) parameters;
+    Write(" Введите колчество строк массива  ");
+    parameters.rows=Convert.ToInt32(ReadLine());
+    Write(" Введите количество столбцов ");
+    parameters.cols=Convert.ToInt32(ReadLine());
+    return parameters;
+}
+
+int [,] FillArrayRamdom(int rows, int cols, int minValues, int maxValues)
+{
+    int[,] array= new int[rows,cols];
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j < array.GetLength(1); j++)
+            array[i,j]=new Random().Next(minValues,maxValues+1);
+    }   
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    WriteLine("Массив ");
+    for(int i=0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j < array.GetLength(1); j++)
+            Write($" {array[i,j]}\t "); 
+        WriteLine();
+    }  
+}
+
+double[] ArithmeticMean(int[,] array)
+{
+    double[] mean=new double[array.GetLength(1)];
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        for(int i=0; i<array.GetLength(0); i++)
+        {
+            mean[j]+=array[i,j];
+        }
+        mean[j]=mean[j]/array.GetLength(0);
+    }
+    return mean;
+}
+
+(int rows, int cols) parametersWork=ParametersRandomArray();
+int[,] massiv=FillArrayRamdom(parametersWork.rows, parametersWork.cols, 0, 10);
+PrintArray(massiv);
+double[] meanWork=ArithmeticMean(massiv);
+WriteLine(" Среднее значение элементов по столбцам ");
+for(int i=0; i<meanWork.Length; i++)
+{
+    Write($" {meanWork[i]:f} \t ");
+}
+WriteLine();
